989ab321597ae9d99d5a95d5dca9efdfc22bfd3b
diak <diak@mesterlevel.lo>
2026-02-25 11:24:37 +0100

    Átírtunk pár dolgot

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     credentials: true
 }))
 
@@ -64,9 +64,9 @@ function auth(req, res, next) {
 
 app.post('/regisztracio', async (req, res) => {
     const { email, felhasznalonev, jelszo, admin } = req.body;
-
+    console.log(req.body);
     // bemeneti adatok ellenõrzése
-    if (!email || !felhasznalonev || !jelszo || !admin) {
+    if (!email || !felhasznalonev || !jelszo || !(admin===0|| admin===1)) {
         return res.status(400).json({ message: "Hiányos bemeneti adatok" })
     }
 
